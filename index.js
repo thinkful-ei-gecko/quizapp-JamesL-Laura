2ceb1b293c42d28ee641ca3c67657aace390db68
laura <laura.j.elias@gmail.com>
2019-08-16 11:49:35 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const questionBank = [
+  {
+    question: 'What is the nickname of Minnesota?',
+    answers: ['Sunshine State', 'The Tundra', 'Land of 10,000 Lakes', 'Star of the North'],
+    correctAnswer: 'Land of 10,000 Lakes' 
+  }
+  {
+    question: 'Which state has the most lakes?',
+    answers: ['Minnesota', 'Wisconsin', 'California', 'Alaska'],
+    correctAnswer: 'Alaska'
+  }
+  {
+    question: 'What was the original name of St. Paul?',
+    answers: ['St. Peter', 'Pig\'s Eye', 'Paul Bunyan', 'Paul\'s Landing'],
+    correctAnswer: 'Pig\'s Eye'
+  }
+  {
+    question: 'How many times can you fit the Yankee Stadium into the Mall of America?',
+    answers: ['3', '5', '7', '9'],
+    correctAnswer: '9'
+  }
+  {
+    question: 'Will Minnesota be your next vacation destination?',
+    answers: ['Yes', 'YES', 'Yes!', 'Definitely'],
+    correctAnswer: '(all)'
+  }
+]
+
